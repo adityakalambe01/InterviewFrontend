6327be197fdd42ea47429616ad1090ba1ce96d13
Aditya Kalambe <adityakalambe20@gmail.com>
2025-03-13 00:15:05 +0530

    Update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   AUTH_API_URL: 'https://admin.liveexamcenter.in/api/auth/login',
-  API_URL: 'http://localhost:5050'
+  API_URL: 'https://interview-6y59.onrender.com'
 };
